4fef1c0d0a0f4ac68b34ae977c2ba32ac6fd06c6
ejjays <christsonalloso021@gmail.com>
2024-10-21 14:22:11 +0800

    Update MeetingRoom.tsx

--- components/MeetingRoom.tsx
+++ components/MeetingRoom.tsx
@@ -83,7 +83,9 @@ const MeetingRoom = () => {
               ))}
             </DropdownMenuContent>
           </DropdownMenu>
-          <CallStatsButton className="hidden lg:block" />
+          <div className="hidden lg:block">
+            <CallStatsButton />
+          </div>
         </div>
         <button onClick={() => setShowParticipants((prev) => !prev)}>
           <div className="cursor-pointer rounded-2xl bg-[#19232d] px-4 py-2 hover:bg-[#4c535b]">
