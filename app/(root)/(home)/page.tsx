77c67c882ca0e418b08b64f1a133c0c30258c0d7
ejjays <christsonalloso021@gmail.com>
2024-10-21 23:37:07 +0800

    Update page.tsx

--- app/(root)/(home)/page.tsx
+++ app/(root)/(home)/page.tsx
@@ -27,7 +27,7 @@ const Home = () => {
     <section className="flex size-full flex-col gap-5 text-white">
       <div className="h-[303px] w-full rounded-[20px] bg-hero bg-cover">
         <div className="flex h-full flex-col justify-between max-md:px-5 max-md:py-8 lg:p-11">
-          <h2 className="glassmorphism max-w-[273px] rounded py-2 text-center text-base font-normal">
+          <h2 className="glassmorphism max-w-[273px] rounded py-2 text-left text-base font-normal ml-4">
             Cellgroup Saturday at 9:00 PM
           </h2>
           <div className="flex flex-col gap-2">
