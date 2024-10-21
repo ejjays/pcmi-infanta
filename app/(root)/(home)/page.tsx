d4517e4e199c99fcc02a7756b751b50370a6ac93
ejjays <christsonalloso021@gmail.com>
2024-10-21 21:48:26 +0800

    Update page.tsx

--- app/(root)/(home)/page.tsx
+++ app/(root)/(home)/page.tsx
@@ -3,14 +3,13 @@ import MeetingTypeList from '@/components/MeetingTypeList';
 const Home = () => {
   // Set time zone to Asia/Manila
   const now = new Date();
-  const utcOffset = -9 * 60; // UTC+9
+  const utcOffset = 8 * 60; // UTC+8
   const localTime = new Date(now.getTime() + utcOffset * 60000);
-  
-  // Adjust time 5 hours earlier
-  const adjustedTime = new Date(localTime.getTime() - 7 * 60 * 60 * 1000); // Subtracting 7 hours
 
+  // Adjust the time as needed (no further adjustments, just local time)
+  const adjustedTime = new Date(localTime.getTime()); // No further adjustments
   // Add 2 days to the date
-  const adjustedDate = new Date(localTime.getTime() + 1 * 24 * 60 * 60 * 1000); // Adding 1 days
+  const adjustedDate = new Date(localTime.getTime() + 2 * 24 * 60 * 60 * 1000); // Adding 2 days
 
   const time = adjustedTime.toLocaleTimeString('en-PH', { hour: '2-digit', minute: '2-digit' });
   const date = new Intl.DateTimeFormat('en-PH', { dateStyle: 'full' }).format(adjustedDate);
@@ -20,15 +19,14 @@ const Home = () => {
       <div className="h-[303px] w-full rounded-[20px] bg-hero bg-cover">
         <div className="flex h-full flex-col justify-between max-md:px-5 max-md:py-8 lg:p-11">
           <h2 className="glassmorphism max-w-[273px] rounded py-2 text-center text-base font-normal">
-  Cellgroup Saturday at 9:00 PM
-</h2>
+            Cellgroup Saturday at 9:00 PM
+          </h2>
           <div className="flex flex-col gap-2">
             <h1 className="text-4xl font-extrabold lg:text-7xl">{time}</h1>
             <p className="text-lg font-medium text-sky-1 lg:text-2xl">{date}</p>
           </div>
         </div>
       </div>
-
       <MeetingTypeList />
     </section>
   );
