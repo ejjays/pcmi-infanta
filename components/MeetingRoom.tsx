62e79e8579199081d0f6af5f322a7c699793638e
ejjays <christsonalloso021@gmail.com>
2024-11-14 15:58:05 +0800

    Update MeetingRoom.tsx

--- components/MeetingRoom.tsx
+++ components/MeetingRoom.tsx
@@ -4,7 +4,6 @@ import { useRouter } from 'next/navigation';
 import {
   CallControls,
   CallParticipantsList,
-  CallStatsButton,
   CallingState,
   PaginatedGridLayout,
   SpeakerLayout,
@@ -84,12 +83,13 @@ const MeetingRoom = () => {
             ))}
           </DropdownMenuContent>
         </DropdownMenu> */}
-        <CallStatsButton />
-        {/* <button onClick={() => setShowParticipants((prev) => !prev)}>
+        {/* Hide CallStatsButton */}
+        {/* <CallStatsButton /> */}
+        <button onClick={() => setShowParticipants((prev) => !prev)}>
           <div className="cursor-pointer rounded-2xl bg-[#19232d] px-4 py-2 hover:bg-[#4c535b]">
             <Users size={20} className="text-white" />
           </div>
-        </button> */}
+        </button>
         <EndCallButton />
       </div>
     </section>
