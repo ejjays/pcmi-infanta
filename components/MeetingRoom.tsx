1d67e89d6d9b8b256657a454ae2eb006985cde3d
ejjays <christsonalloso021@gmail.com>
2024-11-17 10:11:15 +0800

    Update MeetingRoom.tsx

--- components/MeetingRoom.tsx
+++ components/MeetingRoom.tsx
@@ -40,11 +40,23 @@ const MeetingRoom = () => {
   const CallLayout = () => {
     switch (layout) {
       case 'grid':
-        return <PaginatedGridLayout className="grid-layout" />;
+        return (
+          <div className="grid-layout">
+            <PaginatedGridLayout />
+          </div>
+        );
       case 'speaker-right':
-        return <SpeakerLayout participantsBarPosition="left" className="speaker-layout" />;
+        return (
+          <div className="speaker-layout">
+            <SpeakerLayout participantsBarPosition="left" />
+          </div>
+        );
       default:
-        return <SpeakerLayout participantsBarPosition="right" className="speaker-layout" />;
+        return (
+          <div className="speaker-layout">
+            <SpeakerLayout participantsBarPosition="right" />
+          </div>
+        );
     }
   };
 
@@ -54,19 +66,46 @@ const MeetingRoom = () => {
         <div className="flex size-full max-w-[1000px] items-center">
           <CallLayout />
         </div>
+        <div
+          className={cn('h-[calc(100vh-86px)] hidden ml-2', {
+            'show-block': showParticipants,
+          })}
+        >
+          <CallParticipantsList onClose={() => setShowParticipants(false)} />
+        </div>
       </div>
       {/* video layout and call controls */}
       <div className="fixed bottom-5 flex w-full flex-wrap items-center justify-center gap-5 scale-90">
         <CallControls onLeave={() => router.push(`/`)} />
-        {/* Hide CallStatsButton */}
-        {/* <CallStatsButton /> */}
-        {/* Hide Participants Button */}
-        {/* <button onClick={() => setShowParticipants((prev) => !prev)}>
+
+        <DropdownMenu>
+          <div className="flex items-center">
+            <DropdownMenuTrigger className="cursor-pointer rounded-2xl bg-[#19232d] px-4 py-2 hover:bg-[#4c535b]">
+              <LayoutList size={20} className="text-white" />
+            </DropdownMenuTrigger>
+          </div>
+          <DropdownMenuContent className="border-dark-1 bg-dark-1 text-white">
+            {['Grid', 'Speaker-Left', 'Speaker-Right'].map((item, index) => (
+              <div key={index}>
+                <DropdownMenuItem
+                  onClick={() =>
+                    setLayout(item.toLowerCase() as CallLayoutType)
+                  }
+                >
+                  {item}
+                </DropdownMenuItem>
+                <DropdownMenuSeparator className="border-dark-1" />
+              </div>
+            ))}
+          </DropdownMenuContent>
+        </DropdownMenu>
+        <CallStatsButton />
+        <button onClick={() => setShowParticipants((prev) => !prev)}>
           <div className="cursor-pointer rounded-2xl bg-[#19232d] px-4 py-2 hover:bg-[#4c535b]">
             <Users size={20} className="text-white" />
           </div>
-        </button> */}
-        <EndCallButton />
+        </button>
+        {!isPersonalRoom && <EndCallButton />}
       </div>
     </section>
   );
