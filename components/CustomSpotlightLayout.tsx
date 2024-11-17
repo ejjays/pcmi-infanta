b2e9d5c52cfbc2a9493d64b37e4b81f5ea451410
ejjays <christsonalloso021@gmail.com>
2024-11-17 09:02:07 +0800

    Update components

---
+++ components/CustomSpotlightLayout.tsx
@@ -0,0 +1,31 @@
+import React from 'react';
+import { useCallStateHooks } from '@stream-io/video-react-sdk';
+
+const CustomSpotlightLayout = () => {
+  const { useCallParticipants } = useCallStateHooks();
+  const participants = useCallParticipants();
+
+  const mainParticipant = participants[0]; // Assuming the first participant is the spotlight
+  const otherParticipants = participants.slice(1);
+
+  return (
+    <div className="spotlight-layout">
+      {mainParticipant && (
+        <div className="main-participant">
+          {/* Main participant video stream */}
+          <video autoPlay playsInline ref={mainParticipant.videoRef} />
+        </div>
+      )}
+      <div className="other-participants">
+        {otherParticipants.map(participant => (
+          <div key={participant.id} className="participant">
+            {/* Other participants video stream */}
+            <video autoPlay playsInline ref={participant.videoRef} />
+          </div>
+        ))}
+      </div>
+    </div>
+  );
+};
+
+export default CustomSpotlightLayout;
