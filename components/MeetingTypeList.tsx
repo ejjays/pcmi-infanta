c0ab3e208fcb6357acae20399395431c0eab2c96
pcmi-infanta <pcminfanta@gmail.com>
2025-02-18 20:10:45 +0800

    TEST if Ill get admin

--- components/MeetingTypeList.tsx
+++ components/MeetingTypeList.tsx
@@ -51,7 +51,8 @@ const MeetingTypeList = () => {
 
 const ALLOWED_ADMIN_IDS = [
   "user_2sABqr7qHlfl0Sf3Goi4UAYk1Z7",
-  "user_2qbNobut11rUklLIMzmd5nQ8yc1"
+  "user_2qbNobut11rUklLIMzmd5nQ8yc1",
+  "user_2qaEXfvaq5TCAuIhBuH29ng3dtc"
 ];
 
   const createMeeting = async () => {
