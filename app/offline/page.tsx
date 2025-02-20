83a2cb7b62c5e88ba4b56c7201cd8995a27dfd92
pcmi-infanta <pcminfanta@gmail.com>
2025-02-20 21:58:23 +0800

    Auto commit

--- app/offline/page.tsx
+++ app/offline/page.tsx
@@ -1,7 +1,7 @@
 export default function Offline() {
   return (
 <div className="flex-center min-h-screen flex-col gap-4 bg-dark-1 text-white">
-  <h1 className="text-4xl font-bold">You&apos;re Offline</h1>
+  <h1 className="text-4xl font-bold">You&apos;re Offline EJ POGI!</h1>
   <p className="text-lg">Please check your internet connection</p>
 </div>
   );
