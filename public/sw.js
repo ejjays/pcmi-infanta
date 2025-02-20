11d97464bca26499dc213741dbcc81d1a7c00455
pcmi-infanta <pcminfanta@gmail.com>
2025-02-20 22:45:52 +0800

    Auto commit

--- public/sw.js
+++ public/sw.js
@@ -1,7 +1,6 @@
 const CACHE_NAME = 'pcmi-cache-v2-' + new Date().getTime();  
 const urlsToCache = [
   '/',
-  '/offline',
   '/manifest.json',
   '/icons/icon-192x192.png',
   '/icons/icon-512x512.png',
@@ -19,15 +18,7 @@ self.addEventListener('install', (event) => {
 self.addEventListener('fetch', (event) => {
   event.respondWith(
     fetch(event.request).catch(() => {
-      return caches.match(event.request).then((response) => {
-        if (response) {
-          return response;
-        }
-        if (event.request.mode === 'navigate') {
-          return caches.match('/offline');
-        }
-        return Promise.reject('no-match');
-      });
+      return caches.match(event.request);
     })
   );
 });
