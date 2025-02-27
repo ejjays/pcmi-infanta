de657e153a291f7de6c3f6e185c9c4987cdf458a
pcmi-infanta <pcminfanta@gmail.com>
2025-02-27 12:54:10 +0800

    Auto commit

--- next.config.mjs
+++ next.config.mjs
@@ -10,29 +10,33 @@ const pwaConfig = withPWA({
   fallbacks: {
     document: '/offline',
   },
-  runtimeCaching: [{
-    urlPattern: /^https?.*/,
-    handler: 'NetworkFirst', 
-    options: {
-      cacheName: 'offline-cache',
-      expiration: {
-        maxEntries: 200,
-        maxAgeSeconds: 30 * 24 * 60 * 60 
-      },
-      networkTimeoutSeconds: 10,
-      plugins: [
-        {
-          handlerDidError: async () => await caches.match('/offline'),
+  runtimeCaching: [
+    {
+      urlPattern: /^https?.*/,
+      handler: 'NetworkFirst',
+      options: {
+        cacheName: 'offline-cache',
+        expiration: {
+          maxEntries: 200,
+          maxAgeSeconds: 30 * 24 * 60 * 60, // 30 days
         },
-      ], 
-    }, 
-  }], 
+        networkTimeoutSeconds: 10,
+        plugins: [
+          {
+            handlerDidError: async () => await caches.match('/offline'),
+          },
+        ],
+      },
+    },
+  ],
   buildExcludes: [/middleware-manifest.json$/],
   scope: '/',
 });
 
 const nextConfig = {
   ...pwaConfig,
+  reactStrictMode: true,
+  swcMinify: true,
   images: {
     remotePatterns: [
       {
@@ -40,6 +44,13 @@ const nextConfig = {
         hostname: 'img.clerk.com',
       },
     ],
+  },
+  webpack: (config) => {
+    config.externals.push({
+      'utf-8-validate': 'commonjs utf-8-validate',
+      'bufferutil': 'commonjs bufferutil',
+    });
+    return config;
   },
 };
 
