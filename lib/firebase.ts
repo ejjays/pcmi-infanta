42551a1485026496339e9af8e8222098a3411484
ejjays <christsonalloso021@gmail.com>
2025-03-21 13:10:50 +0800

    Auto commit

--- lib/firebase.ts
+++ lib/firebase.ts
@@ -6,7 +6,7 @@ const firebaseConfig = {
   apiKey: "AIzaSyA80oK9bOoaXYVAknMIANx1xZ9-m7MuTRY",
   authDomain: "mediashare-6eb03.firebaseapp.com",
   projectId: "mediashare-6eb03",
-  storageBucket: "mediashare-6eb03.firebasestorage.app",
+  storageBucket: "mediashare-6eb03.appspot.com",
   messagingSenderId: "289581748973",
   appId: "1:289581748973:web:87f5460485b16655ac32e4",
   measurementId: "G-E8L3LTXJBK"
